125c834cd7fa19309ba7b2f93585384c45b49389
Ondrej Blazek <ondrej@kezeri.com>
2019-07-23 23:44:18 -0400

    Material wip

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
+import { MaterialModule } from '@app/app.material.module';
 import { OverviewComponent } from './overview/overview.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     HomeRoutingModule
   ],
   declarations: [
